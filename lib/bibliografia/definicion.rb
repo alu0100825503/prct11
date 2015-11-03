09d68263ccdc700aff1152c52ad2bd7c36cd3a25
alu0100825503 <alu0100825503@ull.edu.es>
2015-11-03 19:42:27 +0000

    Escritos los ficheros de expectativa y definición de la clase Bibliograph

---
+++ lib/bibliografia/definicion.rb
@@ -0,0 +1,39 @@
+class Bibliograph
+    attr_reader :autor, :titulo, :serie, :editorial, :edicion, :fecha, :isbn
+    
+    def initialize(autor, titulo, serie, editorial, edicion, fecha, isbn)
+        @autor, @titulo, @serie, @editorial, @edicion, @fecha, @isbn = autor, titulo, serie, editorial, edicion, fecha, isbn 
+    end    
+    def get_autores
+        vector_autores = @autor
+        return vector_autores
+    end    
+    def get_titulo
+        titulo = @titulo 
+        return titulo
+    end 
+    def get_serie 
+        serie = @serie
+        return serie
+    end
+    def get_editorial
+        editorial = @editorial
+        return editorial
+    end    
+    def get_edicion
+        edicion = @edicion
+        return edicion
+    end    
+    def get_fecha
+        fecha = @fecha
+        return fecha
+    end    
+    def get_isbn
+        vector_isbn = @isbn
+        return vector_isbn
+    end    
+    def get_reference_formatted
+        vector_reference =  [@autor, @titulo, @serie, @editorial, @edicion, @fecha, @isbn]
+        return vector_reference
+    end    
+end
