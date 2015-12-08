db9b980e74c66ee2da05255bf3b08df8227c020e
alu0100825503 <alu0100825503@ull.edu.es>
2015-12-08 11:49:20 +0000

    Cumplida la expectativa para el to_s de un libro

--- spec/linkedlist_spec.rb
+++ spec/linkedlist_spec.rb
@@ -1,16 +1,16 @@
 #!enconding utf8
 
 require "./lib/linkedlist.rb"
-require "./lib/bibliografia/definicion.rb"
+require "./lib/bibliografia/reference.rb"
 
 describe Linkedlist do 
     before :each do
-        @l1 = Bibliograph.new(['Dave Thomas', 'Andy Hunt'], 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide', 'The Facets of Ruby', 'Pragmatic Bookshelf', '4 edition', 'July 7, 2013', ['978-1937785499', '1937785491'])
-        @l2 = Bibliograph.new('Scott Chacon', 'Pro Git 2009th Edition', 'Pro', 'Apress', '2009 edition', 'August 27, 2009', ['978-1430218333', '1430218339'])
-        @l3 = Bibliograph.new(['David Flanagan', 'Yukihiro Matsumoto'], 'The Ruby Programming Language', nil, 'O’Reilly Media', '1 edition', 'February 4, 2008', ['0596516177', '978-0596516178'])
-        @l4 = Bibliograph.new(['David Chelimsky', 'Dave Astels', 'Bryan Helmkamp', 'Dan North', 'Zach Dennis', 'Aslak Hellesoy'], 'The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends', 'The Facets of Ruby', 'Pragmatic Bookshelf', '1 edition', 'December 25, 2010', ['1934356379', '978-1934356371'])
-        @l5 = Bibliograph.new('Richard E. Silverman', 'Git Pocket Guide', nil, 'O’Reilly Media', '1 edition', 'August 2, 2013', ['1449325866', '978-1449325862'])
-    
+        @b1 = Bibliograph.new('Scott Chacon', 'Pro Git 2009th Edition', 'August 27, 2009')
+        @l1 = Libro.new('Scott Chacon', 'Pro Git 2009th Edition', 'Pro', 'Apress', '2009 edition', 'August 27, 2009', ['978-1430218333', '1430218339'])
+        @p1 = Articulo_periodico.new('Jesús Monedero', 'Idiotas o ciudadanos', '29 junio 2014', 'Diario Unidad', [21, 24])
+        @r1 = Articulo_revista.new('Lucas Torre Champsour', 'Documentos sobre la música en la catedral de Las Palmas', 'El Museo Canario', '3 marzo 2006', [353,454])
+        @d1 = Documento_electronico.new('Andrew Harnack', 'Beyond the MLA handbook', 'en línea', 'Chicago: The University of Chicago Press', '4 abril 1997', '<http://falcon.eku.edu/honors/ beyond-mla/>') 
+  
         @list = Linkedlist.new()
     end
 
--- spec/referencesList.rb
+++ spec/referencesList.rb
@@ -1,9 +1,32 @@
-require "spec_helper.rb"
+require './spec_helper.rb'
+require '../lib/linkedlist.rb'
+require '../lib/bibliografia/reference.rb'
 
-
-describe References_APA do
+describe Linkedlist do
     before :each do
-       @reference_list = References_APA.new
-       @libro = Libro.new('Scott Chacon', 'Pro Git 2009th Edition', 'Pro', 'Apress', '2009 edition', 'August 27, 2009', ['978-1430218333', '1430218339'])
+        @b1 = Bibliograph.new('Scott Chacon', 'Pro Git 2009th Edition', 'August 27, 2009')
+        @l1 = Libro.new('Scott Chacon', 'Pro Git 2009th Edition', 'Pro', 'Apress', '2009 edition', 'August 27, 2009', ['978-1430218333', '1430218339'])
+        @p1 = Articulo_periodico.new('Jesús Monedero', 'Idiotas o ciudadanos', '29 junio 2014', 'Diario Unidad', [21, 24])
+        @r1 = Articulo_revista.new('Lucas Torre Champsour', 'Documentos sobre la música en la catedral de Las Palmas', 'El Museo Canario', '3 marzo 2006', [353,454])
+        @d1 = Documento_electronico.new('Andrew Harnack', 'Beyond the MLA handbook', 'en línea', 'Chicago: The University of Chicago Press', '4 abril 1997', '<http://falcon.eku.edu/honors/ beyond-mla/>') 
+    
+        @lista = Linkedlist.new
+    end
+    
+    it "Se almacenan correctamente las referencias en la lista" do
+        @lista.insert_end(@l1) 
+        @lista.insert_end(@p1)
+        @lista.insert_end(@r1)
+        
+        expect(@lista.extract_begin).to eq @l1
     end    
+    
+    it "Se muestra correctamente la información de un libro" do
+        expect(@l1.to_s).to eq 'Chacon, S. (2009). Pro Git 2009th Edition (2009 edition). Apress'
+    end    
+    
+    it "Se muestra correctamente la información de un artículo de periódico" do
+        expect(@p1.to_s).to eq false
+    end    
+    
 end        
